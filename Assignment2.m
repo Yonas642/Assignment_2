ab55229f39640e2b87e267bc45af98080343432e
Yonas642 <93714527+Yonas642@users.noreply.github.com>
2021-12-04 23:41:37 +0200

    First commit

--- Assignment2.m
+++ Assignment2.m
@@ -1,46 +1,66 @@
-% x = [theta_dot; d_dot];
+
+clc
+close all
 a = 0.1;
 b = 0.2;
-w = 1;
-d  = 0.28;
-phi = -1;
-phi_1 = deg2rad(30);
-t=linspace(0,1,101);
-phi = -1;
-theta  = deg2rad(14.4775);
-
-phi_2 = (pi/6)+w*t;
-
-% set a reasonable starting point
-x0 = [0; b + a];
+w=-1;
+t = linspace(0,1,101) % loop for t 
+phi = pi/6+w*t;
+y0 = [0; b + a];       % set a reasonable starting point
+J = @(y) jacobian(y, b);
+eps = 1e-9;
+ Y=[];  %array
 
 % create function handles
-F = @(x) constraint(a, b, phi_1,d,theta);
-Jt = @(x,t) jacobian(x,a,b,theta,phi_1,phi);
 
-eps = 1e-9;
-[x, iteration_counter] = NR_method(F,J, x0, eps);
-
-fprintf('\n\tMechanism valid position is for d_dot = %.3g m and theta_dot = %g deg\n\n', ...
-    x(2), rad2deg(x(1)));
-
-function f = constraint(a, b, phi_1,d,theta)
-
-f = [a * cos(phi_1) + b * cos(theta) - d
-    a * sin(phi_1) - b * sin(theta) ];
-end
-
-function f = jacobian(x,a,b,theta,phi_2,phi)
-theta_dot = x(1);
-d_dot = x(2);
-
-f = [-a *phi*sin(phi_2), -b*theta_dot*sin(theta), -d_dot
-    a *phi*cos(phi_2), -b*theta_dot*cos(theta), 0];
-for t = t +dt,t < t(n)
-    f(x,t) = [-a *phi_dot*sin(phi_1), -b*theta_dot*sin(theta), -d_dot
-    a *phi_dot*cos(phi_1), -b*theta_dot*cos(theta), 0];
+for i=1:length(phi)
     
-    x0 = x + dt*x
+F = @(y) constraint(y, a, b, phi(i));
+x = NR_method(F, J, y0, eps)
+Y=[Y x];
 end
 
-end  
+theta=(Y(1,:));
+d=Y(2,:);
+figure  
+plot(t, d,'r-')
+title('Displacement vs time');
+xlabel('Time - t (s)')
+ylabel('Displacement (d) (m) ')
+
+figure 
+plot(t, rad2deg(theta),'m-.')
+title('Theta vs time');
+xlabel('Time t (s)')
+ylabel('Theta (\theta) (\circ) ')
+
+g = gradient(theta)./gradient(t) ;   
+
+% time derivative of angle theta 
+figure
+plot(t,g,'r-');
+title('Angular velocity vs time');
+xlabel('Time t (s)')
+ylabel('Angular velocity $\dot{d}$ (rad/s)', 'Interpreter','latex')
+g = gradient(d)./gradient(t) ;  
+
+% time derivative of displacement 
+figure 
+plot(t,g,'m-.');
+title('Velocity vs time');
+xlabel('Time-t (s)')
+ylabel('Velocity $\dot{d}$ (m/s)')
+   
+function P = constraint(y, a, b, phi)  % constraints
+
+theta = y(1);
+d = y(2);
+P = [a * cos(phi) + b * cos(theta) - d;
+    a * sin(phi) - b * sin(theta)];
+end
+
+function P = jacobian(y, b)   % Jacobian matrix
+theta = y(1);
+P = [-b * sin(theta), -1
+    -b * cos(theta), 0];
+end
